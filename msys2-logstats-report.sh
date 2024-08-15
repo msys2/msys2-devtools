3828a0a83c7c17f6178f5c0d6fb46258bb48b2c7
Christoph Reiter <reiter.christoph@gmail.com>
2024-08-15 20:06:28 +0200

    logstats: refactor; add script for creating report

---
+++ msys2-logstats-report.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# journalctl --since "7 days ago" --output=cat > logs.txt
+
+set -e
+
+LOGS=logs.txt
+OUTPUT=logs-report.md
+
+date -I > "$OUTPUT"
+
+echo '<details><summary>CI vs non-CI requests</summary>' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+./msys2-logstats --show-summary "$LOGS" >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '</details>' >> "$OUTPUT"
+
+echo '<details><summary>All requests</summary>' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+./msys2-logstats "$LOGS" >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '</details>' >> "$OUTPUT"
+
+echo '<details><summary>Without CI/cloud requests</summary>' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+./msys2-logstats --skip-ci "$LOGS" >> "$OUTPUT"
+echo '```' >> "$OUTPUT"
+echo '' >> "$OUTPUT"
+echo '</details>' >> "$OUTPUT"
