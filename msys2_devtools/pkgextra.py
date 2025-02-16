1e266359723fdefdf10549a95fd72c1b6cf128e3
Christoph Reiter <reiter.christoph@gmail.com>
2025-02-16 19:06:24 +0100

    pkgextra: sync with msys2-web

--- msys2_devtools/pkgextra.py
+++ msys2_devtools/pkgextra.py
@@ -1,33 +1,33 @@
 from pydantic import BaseModel, Field
-from typing import Dict, Optional, Sequence, Union, Collection
+from typing import Sequence, Collection
 
 
 class PkgExtraEntry(BaseModel):
     """Extra metadata for a PKGBUILD"""
 
-    internal: bool = Field(default=False)
-    """If the package is MSYS2 internal or just a meta package"""
-
-    references: Dict[str, list[Optional[str]]] = Field(default_factory=dict)
+    references: dict[str, list[str | None]] = Field(default_factory=dict)
     """References to third party repositories"""
 
-    changelog_url: Optional[str] = Field(default=None)
+    changelog_url: str | None = Field(default=None)
     """A NEWS file in git or the github releases page.
     In case there are multiple, the one that is more useful for packagers
     """
 
-    documentation_url: Optional[str] = Field(default=None)
+    documentation_url: str | None = Field(default=None)
     """Documentation for the API, tools, etc provided, in case it's a different
     website"""
 
-    repository_url: Optional[str] = Field(default=None)
+    repository_url: str | None = Field(default=None)
     """Web view of the repository, e.g. on github or gitlab"""
 
-    issue_tracker_url: Optional[str] = Field(default=None)
+    issue_tracker_url: str | None = Field(default=None)
     """The bug tracker, mailing list, etc"""
 
-    pgp_keys_url: Optional[str] = Field(default=None)
+    pgp_keys_url: str | None = Field(default=None)
     """A website containing which keys are used to sign releases"""
+
+    ignore_vulnerabilities: list[str] = Field(default_factory=list)
+    """List of CVEs or GHSAs that are either not relevant or not fixable"""
 
 
 def convert_mapping(array: Sequence[str]) -> dict[str, list[str | None]]:
@@ -43,7 +43,7 @@ def convert_mapping(array: Sequence[str]) -> dict[str, list[str | None]]:
     return converted
 
 
-def extra_to_pkgextra_entry(data: Dict[str, Union[str, Collection[str]]]) -> PkgExtraEntry:
+def extra_to_pkgextra_entry(data: dict[str, str | Collection[str]]) -> PkgExtraEntry:
     mappings = ["references"]
 
     data = dict(data)
