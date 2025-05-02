04d420fa174027f72abd85117dcea08ef4153cb3
Christoph Reiter <reiter.christoph@gmail.com>
2025-05-02 10:15:08 +0200

    Move from zstandard to pyzstd
    It will be part of the stdlib in 3.14, so lets move to that.
    https://peps.python.org/pep-0784/
    
    and add some tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,7 @@
-from msys2_devtools.db import parse_desc
+import io
+import tarfile
+import pytest
+from msys2_devtools.db import parse_desc, ExtTarFile
 
 
 def test_parse_desc():
@@ -27,3 +30,28 @@ libiconv
     assert desc["%BASE%"] == ["libarchive"]
     assert desc["%VERSION%"] == ["3.7.4-1"]
     assert desc["%DEPENDS%"] == ["gcc-libs", "libbz2", "libiconv"]
+
+
+def test_zstd():
+    fileobj = io.BytesIO()
+    with ExtTarFile.open(fileobj=fileobj, mode='w:zstd') as tar:
+        data = "Hello world!".encode('utf-8')
+        info = tarfile.TarInfo("test.txt")
+        info.size = len(data)
+        tar.addfile(info, io.BytesIO(data))
+    fileobj.seek(0)
+
+    with ExtTarFile.open(fileobj=fileobj, mode='r') as tar:
+        assert len(tar.getnames()) == 1
+        assert tar.getnames()[0] == "test.txt"
+        assert tar.extractfile("test.txt").read() == b"Hello world!"
+
+
+def test_zstd_invalid():
+    with pytest.raises(tarfile.ReadError):
+        fileobj = io.BytesIO()
+        ExtTarFile.open(fileobj=fileobj, mode='r')
+
+    with pytest.raises(tarfile.ReadError):
+        fileobj = io.BytesIO(b"\x00\x00\x00")
+        ExtTarFile.open(fileobj=fileobj, mode='r')
